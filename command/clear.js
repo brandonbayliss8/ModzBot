c6e279ffa7250aa3dd0579737d7f66c71ff5af0f
ModzOn <modzongaming@gmail.com>
2017-04-28 17:57:42 +0200

    Update clear.js

--- command/clear.js
+++ command/clear.js
@@ -1,4 +1,3 @@
-const Discord = require("discord.js");
 exports.run = (bot, message, args) => {
   if (message.guild === null) { //DM channel
       message.reply("Go into a server instead and try it again there!") //error message
