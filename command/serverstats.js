f0154fbf3c698a93f5ebccbf5e6cb52d0259684c
ModzOn <modzongaming@gmail.com>
2017-05-01 20:58:00 +0200

    Update serverstats.js

--- command/serverstats.js
+++ command/serverstats.js
@@ -1,11 +1,30 @@
 const Discord = require("discord.js");
 exports.run = (bot, message, args) => {
-  if (message.guild === null) { //DM channel
-      message.reply("Go into a server instead and try it again there!") //error message
-      console.log(`${message.createdAt}: ${message.author.username} tried to get serverinfo in a DM channel`) //console error message
-    } else { //continue with Serverstats
       let name = message.guild.id;
       const embed = new Discord.RichEmbed()
+      let verifLevels = ["None", "Low", "Medium", "(╯°□°）╯︵  ┻━┻"];
+      let region = {
+          "brazil": "Brazil",
+          "eu-central": "Central Europe",
+          "singapore": "Singapore",
+          "us-central": "U.S. Central",
+          "sydney": "Sydney",
+          "us-east": "U.S. East",
+          "us-south": "U.S. South",
+          "us-west": "U.S. West",
+          "eu-west": "Western Europe",
+          "vip-us-east": "VIP U.S. East",
+          "london": "London",
+          "amsterdam": "Amsterdam",
+          "hongkong": "Hong Kong"
+      };
+
+      var emojis;
+      if (message.guild.emojis.size === 0) {
+          emojis = 'None';
+      } else {
+          emojis = message.channel.guild.emojis.map(e => e).join(" ");
+      }
         embed.setColor(3447003)
         .setAuthor(`${bot.guilds.get(name).name}`, bot.guilds.get(name).iconURL)
         .setTitle(`${bot.guilds.get(name).name}` , "Server Info")
@@ -13,15 +32,16 @@ exports.run = (bot, message, args) => {
         .setThumbnail(bot.guilds.get(name).iconURL)
         .addField(`• Server ID`, name, true)
         .addField(`• Server Owner`, `${bot.guilds.get(name).owner}`, true)
-        .addField(`• Server Region`, `${bot.guilds.get(name).region}`, true)
+        .addField(`• Server Region`, region[message.guild.region], true)
+        .addField(`• Verification Levels`, verifLevels[message.guild.verificationLevel], true)
         .addField(`• Created On`, `${bot.guilds.get(name).createdAt}`, true)
         .addField(`• Members`, `${bot.guilds.get(name).memberCount}`, true)
         .addField(`• Default Channel`, `${bot.guilds.get(name).defaultChannel}`, true)
         .addField(`• Channels`, `${bot.guilds.get(name).channels.map(c => c.name).join(", ")}`)
         .addField(`• Roles`, `${bot.guilds.get(name).roles.map(r => r.name).join(", ")}`)
+        .addField(`• Emojis`, `${emojis}`)
         .setFooter(`Generated on ${message.createdAt}`)
 
         message.channel.sendEmbed(embed);
         console.log(`${message.createdAt}: ${message.author.username} requested serverinfo about ${bot.guilds.get(message.guild.id).name}`)
-    }
 }
