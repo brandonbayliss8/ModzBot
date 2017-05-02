54d5d3bdd693ba5e7223e57af7324df83b5c6628
ModzOn <modzongaming@gmail.com>
2017-05-02 15:46:50 +0200

    Update reload.js

--- command/reload.js
+++ command/reload.js
@@ -1,6 +1,6 @@
 exports.run = (bot, message, args) => {
   if (message.author.id !== "264331473308483584") return message.reply(`You don't have permission to run this command!`);
-  if(!args[0]) return message.channel.reply(`Must provide a command name to reload.`);
+  if(!args[0]) return message.channel.sendMessage(`Must provide a command name to reload.`);
   // the path is relative to the *current folder*, so just ./filename.js
   delete require.cache[require.resolve(`./${args[0]}.js`)];
   message.reply(`The command **${args[0]}** has been reloaded`);
