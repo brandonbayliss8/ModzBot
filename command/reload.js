acefe6ec1f70c1cb3bb51af2b7ab1c4550ed3c7a
ModzOn <modzongaming@gmail.com>
2017-05-02 15:29:49 +0200

    Update reload.js

--- command/reload.js
+++ command/reload.js
@@ -1,8 +1,44 @@
+const fs = require("fs");
 exports.run = (bot, message, args) => {
-  if (message.author.id !== "264331473308483584") return message.reply(`You don't have permission to run this command!`);
-  if(!args || args.size < 1) return message.channel.reply(`Must provide a command name to reload.`);
-  // the path is relative to the *current folder*, so just ./filename.js
-  delete require.cache[require.resolve(`./${args[0]}.js`)];
-  message.reply(`The command **${args[0]}** has been reloaded`);
-  console.log(`${message.createdAt}: The command ${args[0]} has been reloaded`);
+   if(message.author.id != "264331473308483584") return;
+  if (args[0] === "all") {
+    bot.log("Reloading all commands");
+    bot.functions.core.loadCommands(bot);
+    return;
+  }
+  let command;
+  if (bot.commands.has(args[0])) {
+    command = args[0];
+  } else if (bot.aliases.has(args[0])) {
+    command = bot.aliases.get(args[0]);
+  }
+  if (!command) {
+    if (args[0].includes(".js")) args[0] = args[0].replace(".js","");
+    fs.stat(`./cmd/${args[0]}.js`, (err, stats) => {
+      if (err) return message.channel.sendMessage(`I cannot find the command: ${args[0]}`);
+      if (stats.isFile()) {
+        message.channel.sendMessage(`Loading New Command: ${args[0]}`)
+        .then(m => {
+          bot.functions.core.reload(bot, args[0])
+          .then(() => {
+            m.edit(`Successfully Loaded: ${args[0]}`);
+          })
+          .catch(e => {
+            m.edit(`Command load failed: ${args[0]}\n\`\`\`${e.stack}\`\`\``);
+          });
+        });
+      }
+    });
+  } else {
+    message.channel.sendMessage(`Reloading: ${command}`)
+    .then(m => {
+      bot.functions.core.reload(bot, command)
+      .then(() => {
+        m.edit(`Successfully reloaded: ${command}`);
+      })
+      .catch(e => {
+        m.edit(`Command reload failed: ${command}\n\`\`\`${e.stack}\`\`\``);
+      });
+    });
+  }
 };
