f72d2f2ec99c31e41544f0aaf9c60b9631f32b54
ModzOn <modzongaming@gmail.com>
2017-05-04 11:06:49 +0200

    ModzBot v1.2.1

--- modzbot.js
+++ modzbot.js
@@ -5,17 +5,8 @@ const moment = require("moment");
 const streamOptions = { seek: 0, volume: 1 };
 const config = require("./config.json"); //contains the prefix and bot token
 const fs = require("fs");
-
-const now = new Date();
- const date = moment(now).format("MMM/DD/YYYY"); //month date and year.
-const time = moment(now).format("H:mm:ss");
-
 const TOKEN = process.env.TOKEN;
-bot.commands = new Discord.Collection();
-bot.aliases = new Discord.Collection();
-bot.commandInhibitors = new Discord.Collection();
-bot.functions = {};
-const prefix = ":";
+const prefix = "::";
 // Load the contents of the `/command/` folder and each file in it.
 fs.readdir("./events/", (err, files) => {
   if (err) return console.error(err);
