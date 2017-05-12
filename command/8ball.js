d71a0c54c56a918b318c152a483d79aa06d7611b
ModzOn <modzongaming@gmail.com>
2017-05-12 20:33:59 +0200

    Update 8ball.js

--- command/8ball.js
+++ command/8ball.js
@@ -1,4 +1,4 @@
 exports.run = (bot, message, args) => {
-  var answ = ["Yes", "No", "Why?", "Stfu human", "Let's find it out!", "Maybe", "I don't know mate", "Definately", "Are you fucking serious", "For sure!", "Kill yourself", "Yes", "No", "Of course!", "Of course not faggot"];
+  var answ = ["Yes", "No", "Why?", "Please stfu human", "Let's find it out!", "Maybe", "I don't know mate", "Definately", "Are you serious?", "For sure!", "Yes", "No", "Of course!", "Of course not"];
     message.channel.sendMessage(answ[Math.floor(Math.random() * answ.length)]);
 }
