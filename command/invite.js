9478736812dc61bfd11598c71b94a53bd399e5f1
ModzOn <modzongaming@gmail.com>
2017-05-15 15:46:01 +0200

    Update invite.js

--- command/invite.js
+++ command/invite.js
@@ -1,4 +1,4 @@
 exports.run = (bot, message, args) => {
-  message.reply("Invite me! https://discordapp.com/oauth2/authorize?client_id=297437352127627264&scope=bot&permissions=66321471")
+  message.reply("Invite me! https://discordapp.com/oauth2/authorize?client_id=297437352127627264&scope=bot&permissions=2136472639")
   console.log(`${message.createdAt}: ${message.author.username} requested the bot\'s invite link`)
 };
