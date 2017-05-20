c3ecd4173dcc1479c4cd0d93647269fc69a8e02c
ModzOn <modzongaming@gmail.com>
2017-05-20 19:34:59 +0200

    Delete reload.js

--- events/reload.js
+++
@@ -1,8 +0,0 @@
-exports.run = (bot, message, args) => {
-  if (message.author.id !== "264331473308483584") return message.reply(`You don't have permission to run this command!`)
-  if(!args || args.size < 1) return message.channel.reply(`Must provide a command name to reload.`);
-  // the path is relative to the *current folder*, so just ./filename.js
-  delete require.cache[require.resolve(`./${args[0]}.js`)];
-  message.reply(`The command **${args[0]}** has been reloaded`);
-  console.log(`${message.createdAt}: The command ${args[0]} has been reloaded`);
-};
