1849b26037a8c62518755b347336721cc19368ce
ModzOn <modzongaming@gmail.com>
2017-05-20 20:56:35 +0200

    Update disable.js

--- command/disable.js
+++ command/disable.js
@@ -1,26 +1,7 @@
-const fs = require("fs");
-      data = JSON.parse(fs.readFileSync('../data/data.json', 'utf8')),
 exports.run = (bot, message, args) => {
   if (!args[0]) return;
   let command = args[0].toLowerCase();
   args = args.join(" ").substring(command.length + 1);
-  /*if (command === "welcome") {
-
-    function disableJoinmessage() {
-        if (!data[message.guild.id]) data[message.guild.id] = {
-            joinMessage: "disabled"
-        };
-        data[message.guild.id].joinMessage = "disabled";
-        let updateValue = JSON.stringify(data, null, 2);
-        fs.writeFileSync('./data/data.json', updateValue);
-        message.reply(":white_check_mark: Welcome message disabled.");
-    }
-    if (message.channel.guild.ownerID === message.author.id) {
-        disableJoinmessage();
-    } else {
-        disableJoinmessage();
-    }
-  }*/
   if (command === "logs") {
     message.reply(`Would you like to disable the logs?\n*This will be canceled in 10 seconds. Type \`yes\` or \`no\`.*`).then(() => {
       message.channel.awaitMessages(m => m.author.id === message.author.id && ['yes', 'no'].includes(m.content), {time: 10000, maxMatches: 1})
