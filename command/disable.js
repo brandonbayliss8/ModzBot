0f4b5ae544fdca8a29e51a3f7913214bf84615e3
ModzOn <modzongaming@gmail.com>
2017-05-20 20:54:32 +0200

    1.2.2. tweaks

--- command/disable.js
+++ command/disable.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-      data = JSON.parse(fs.readFileSync('./data/data.json', 'utf8')),
+      data = JSON.parse(fs.readFileSync('../data/data.json', 'utf8')),
 exports.run = (bot, message, args) => {
   if (!args[0]) return;
   let command = args[0].toLowerCase();
