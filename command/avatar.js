cdfd4e0ff2fefac0234ee2974781a59d75545ba4
ModzOn <modzongaming@gmail.com>
2017-05-23 20:17:27 +0200

    Update avatar.js

--- command/avatar.js
+++ command/avatar.js
@@ -1,3 +1,15 @@
+const Discord = require("discord.js");
 exports.run = (bot, message, args) => {
-  message.reply(message.author.avatarURL); //sends the avatar of the user in the current channel
+  let img = message.mentions.users.first()
+  if (!args[0]) return message.reply(`Please mention a user!`);
+  if (!img) return message.reply(`Please mention a valid user!`);
+  if (img.avatarURL === null) {
+    message.channel.sendMessage("The mentioned user (" + img + ") does not have an avatar!")
+  } else {
+  const embed = new Discord.RichEmbed()
+  .setImage(`${img.avatarURL}`)
+  .setFooter(`Avatar of ${img.username}#${img.discriminator} requested by ${message.author.username}#${message.author.discriminator}`)
+
+  message.channel.sendEmbed(embed);
+  }
 }
