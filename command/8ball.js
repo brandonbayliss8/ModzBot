7782e36a7af8f9ac1ccf14bc7fce9fccee0c9918
ModzOn <modzongaming@gmail.com>
2017-05-23 20:19:02 +0200

    Update 8ball.js

--- command/8ball.js
+++ command/8ball.js
@@ -1,4 +1,8 @@
 exports.run = (bot, message, args) => {
+  let question = args.join(' ');
+  if (question === "kieb") return message.channel.sendMessage("kieb is stronk");
+  if (question === "jetti") return message.channel.sendMessage("no fuk you kieb stronk");
   var answ = ["Yes", "No", "Why?", "Please stfu human", "Let's find it out!", "Maybe", "I don't know mate", "Definately", "Are you serious?", "For sure!", "Yes", "No", "Of course!", "Of course not"];
+  if (!question) return message.reply(`Please ask me a question`)
     message.channel.sendMessage(answ[Math.floor(Math.random() * answ.length)]);
 }
