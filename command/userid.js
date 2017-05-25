b639562a45a609aa1e2582c8c2e3a069c714acfd
ModzOn <modzongaming@gmail.com>
2017-05-25 21:30:21 +0200

    Update userid.js

--- command/userid.js
+++ command/userid.js
@@ -1,5 +1,5 @@
 exports.run = (bot, message, args) => {
   let user = message.mentions.users.first();
-  if !user return message.reply(`User ID for **${message.author.id}#${message.author.discriminator}**: \n\`\`\`${message.author.id}\`\`\``); //sends the userID of the user
+  if (!user) return message.reply(`User ID for **${message.author.id}#${message.author.discriminator}**: \n\`\`\`${message.author.id}\`\`\``); //sends the userID of the user
   message.reply(`User ID for **${user.username}#${user.discriminator}**: \n\`\`\`${user.id}\`\`\``)
 }
