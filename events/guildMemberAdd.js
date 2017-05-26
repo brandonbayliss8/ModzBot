3aa4174b62f8a899d0ffb4f80686a602700b6241
ModzOn <modzongaming@gmail.com>
2017-05-26 10:51:45 +0200

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 exports.run = (bot, member) => {
 
       let guildid = member.guild.id;
-
+  if (guildid === "110373943822540800") return; //blacklist
            let guild = member.guild;
            const embed = new Discord.RichEmbed()
              embed.setColor('#00CE00')
@@ -12,6 +12,6 @@ exports.run = (bot, member) => {
         var msg;
         msg = `Hi ${member}, welcome to ${member.guild.name}!`;
         guild.defaultChannel.sendMessage(msg);
-        console.log(`${member} joined ${member.guild.name}`)
+        console.log(`${member} joined ${member.guild.name}`);
         //bot.channels.get("305057938567987200").sendEmbed(embed);
     }
