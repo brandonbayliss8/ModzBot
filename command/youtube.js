0736229e38bba817c4def5249fcdaa1e483368f8
ModzOn <modzongaming@gmail.com>
2017-06-04 17:58:47 +0200

    Add files via upload

---
+++ command/youtube.js
@@ -0,0 +1,27 @@
+const moment = require("moment");
+      yt = require("ytdl-core");
+      YouTube = require("youtube-node");
+      youTube = new YouTube();
+      youTube.setKey('AIzaSyAz7etLG7SeqL8YKNI0PDpzcRp6HUFF_Ug');
+exports.run = (bot, message, args) => {
+if (!args[0]) return;
+let command = args[0].toLowerCase();
+args = args.join(" ").substring(command.length + 1);
+  if (command === "search") {
+    function searchargs(query) {
+    youTube.search(query, 10, function(error, result) {
+                if (error) {
+                    message.channel.sendMessage(`:no_entry_sign: **Error:**\n${error}`);
+                    console.log(error);
+                } else {
+                    if (!result.items[0]["id"]) return message.reply(":no_entry_sign: **Error:** There was an issue getting that song.");
+                    if (result.items[0]["id"].videoId === undefined) return message.reply(":no_entry_sign: **Error: no videos found!**")
+                    let url = `https://www.youtube.com/watch?v=${result.items[0]["id"].videoId}`;
+                    message.channel.sendMessage(url);
+                }
+            });
+         };
+         let query = message.content.replace(':youtube search', '');
+       searchargs(query);
+     }
+}
