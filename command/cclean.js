5fdba916f83660ed7be807d07236ed9e5b858b03
ModzOn <modzongaming@gmail.com>
2017-06-04 17:43:48 +0200

    Add files via upload

---
+++ command/cclean.js
@@ -0,0 +1,8 @@
+exports.run = (bot, message) => {
+    message.channel.fetchMessages({limit: 75})
+      .then(msgs => {
+        let msg_array = msgs.array().filter(m => m.author.id === bot.user.id);
+        message.channel.bulkDelete(msg_array);
+      });
+    message.channel.sendMessage(":white_check_mark: Commands cleaned!");
+}
