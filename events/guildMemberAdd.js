f8e5a6c9d051e7ffbf267f854da28df8a644e2d8
ModzOn <modzongaming@gmail.com>
2017-06-08 17:33:59 +0200

    Create guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const moment = require("moment");
 exports.run = (bot, member) => {
 
       let guildid = member.guild.id;
@@ -10,7 +11,11 @@ exports.run = (bot, member) => {
 
         var msg;
         msg = `Hi ${member}, welcome to ${member.guild.name}!`;
+        if (guildid === "110373943822540800") return console.log(`Disabled in ${guild}`);
+        const now = new Date();
+        const date = moment(now).format("DD/MMM/YYYY");
+        const time = moment(now).format("H:mm:ss");
         guild.defaultChannel.sendMessage(msg);
-        console.log(`${member} joined ${member.guild.name}`);
+        console.log(`${date}, ${time}: ${member} joined ${member.guild.name}`);
         //bot.channels.get("305057938567987200").sendEmbed(embed);
     }
