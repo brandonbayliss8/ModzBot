8d1b009109ba9666ac29fe6e9bd76a7b7ad878fd
ModzOn <modzongaming@gmail.com>
2017-06-09 19:08:09 +0200

    Create rockpaperscissors.js

--- command/rockpaperscissors.js
+++ command/rockpaperscissors.js
@@ -2,6 +2,6 @@ var rps = ["Rock", "Paper", "Scissors"]
 exports.run = (bot, message, args) => {
   let choice = args.join(" ").toLowerCase();
   if (choice === '') return message.reply("Please specify either rock, paper or scissors.");
-  if (choice !== "rock" && choice !== "paper" && choice !== "scissors") return msg.reply(`Please specify either rock, paper or scissors. ${choice} isn't one of those you fag`);
+  if (choice !== "rock" && choice !== "paper" && choice !== "scissors") return message.reply(`Please specify either rock, paper or scissors. ${choice} isn't one of those you fag`);
     message.reply(rps[Math.floor(Math.random() * rps.length)] + ", I win mother rossija stronk I always win lol \:flag_ru\:");
 }
