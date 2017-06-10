a09e14bc9079a08cc51674c6b6487000050dd7c8
ModzOn <modzongaming@gmail.com>
2017-06-10 19:10:09 +0200

    Create love.js

--- command/love.js
+++ command/love.js
@@ -1,6 +1,6 @@
 exports.run = (bot, message, args) => {
   if (!args[0]) return message.reply(`Please mention someone.`);
-  let person = args[0];
+  let person = args.join(" ");
   let random = Math.floor( Math.random() * 100) + 1;
   message.channel.sendMessage(`There\'s ${random}% love between ${person} and ${message.author.username}`);
 }
