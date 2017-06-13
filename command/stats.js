1348e189b3e5bc7f887d0ad18040768591aa1275
ModzOn <modzongaming@gmail.com>
2017-06-13 18:41:59 +0200

    Create stats.js

--- command/stats.js
+++ command/stats.js
@@ -7,21 +7,20 @@ exports.run = (bot, message, args) => {
          const date = moment(now).format("MMM/DD/YYYY");
          const time = moment(now).format("H:mm:ss");
         const duration = moment.duration(bot.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
-        var quotes = ['modzon sux!', 'omg Brem Djenzzen uses hax!', 'modz hab more subs then pewdiepoop!', 'I luv yuo!']
         const randomNumber = Math.floor(Math.random() * (quotes.length));
       embed.setColor(3447003)
       .setAuthor(`ModzBot Stats`, `http://kuuv.io/i/d035x2P.jpg`)
       .setTitle(`ModzBot Stats`)
-      .setDescription(`${quotes[randomNumber]}`)
+      .setDescription(`am best bot!`)
       .setThumbnail(`http://kuuv.io/i/d035x2P.jpg`)
       .addField(`Lib`, `Discord.js (Javascript)`, true)
-      .addField(`Version`, `1.1.0`, true)
+      .addField(`Version`, `1.2.5`, true)
       .addField(`Uptime`, `${duration}`, true)
       .addField(`Memory`, `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, true)
       .addField(`Users`, `${bot.users.size.toLocaleString()}`, true)
       .addField(`Servers`, `${bot.guilds.size.toLocaleString()}`, true)
       .addField(`Channels`, `${bot.channels.size.toLocaleString()}`, true)
-      .setFooter(`${quotes[randomNumber]} Generated on ${date} at ${time}`)
+      .setFooter(`am best bot! Generated on ${date} at ${time}`)
 
       message.channel.sendEmbed(embed);
 
