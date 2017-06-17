dbbe0aa0053c50f1e9765b2890a9b4dd2f9f7d5d
ModzOn <modzongaming@gmail.com>
2017-06-17 14:35:45 +0200

    Update roast.js

--- command/roast.js
+++ command/roast.js
@@ -13,10 +13,9 @@ exports.run = (bot, message, args) => {
   ];
   const random = Math.floor(Math.random() * roasts.length);
   if (message.author.id === "259758466942369794") return message.reply("**DANGER** *High dosage of chemotherapy, use only as instructed to combat extreme Habnal Cancer* :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: *Developed by Karolinska Institute Research and Development in Solborg, manufactured in Tripolia*");
-  if (message.guild.id === "315531836636004365" && message.author.id !== "264331473308483584") return message.reply("oh nyet not you, you stupid furry go fuck some furries we need to purge folfs");
   if (args[0] === "me") {
     message.reply(roasts[random]);
-  } else if (!args.join("")) {
+  } else if (!args.join(" ")) {
     message.reply("please specify who I need to roast");
 } else {
   message.channel.sendMessage(args.join(" ") + ", " + roasts[random]);
