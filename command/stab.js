bf18cb918689e5c013b0be40b8a61cb8c9249d86
ModzOn <modzongaming@gmail.com>
2017-06-17 12:16:09 +0200

    Update stab.js

--- command/stab.js
+++ command/stab.js
@@ -2,6 +2,6 @@ exports.run = (bot, message, args) => {
   if (message.mentions.users.first() !== undefined) {
     message.reply(`perkele сука блять I stabbed ${message.mentions.users.first()}`);
   } else {
-    message.reply(`perkele сука блять I stabbed someone`);
+    message.reply(`perkele сука блять I stabbed ${args.join(' ')}`);
   }
 };
