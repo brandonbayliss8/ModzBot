1ac3d9f5b9a173d69bae13224ac92b766adfb1f4
ModzOn <modzongaming@gmail.com>
2017-06-19 19:40:32 +0200

    Update ping.js

--- command/ping.js
+++ command/ping.js
@@ -1,5 +1,5 @@
 exports.run = (bot, message, args) => {
-    message.channel.sendMessage("huh")
+    message.channel.send("huh")
       .then(m => {
           m.edit(`:ping_pong: Pong! (took: **${m.createdTimestamp - message.createdTimestamp} ms**)`)
         });
