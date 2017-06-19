efa18c4efc522998e5e3b5637f30c8f9a95a65e0
ModzOn <modzongaming@gmail.com>
2017-06-19 19:42:52 +0200

    Update roast.js

--- command/roast.js
+++ command/roast.js
@@ -5,19 +5,18 @@ exports.run = (bot, message, args) => {
     "Hello Kitty said goodbye to you",
     "You are so fat that the only alphabet letters you know are KFC",
     "You're so fat that you need cheats for Wii Fit",
-    "If God was real, He wouldn't have made something as ugly as you.",
+    "If Got was real, He wouldn't have made something as ugly as you.",
     "You are so stupid that you look at Aliexpress for cheap lifes to buy",
     "I can explain it to you, but you probably won't understand because you're so stupid.",
     "Greenland is hotter then you",
     "You'd probably wish that you don't have such an ugly face."
   ];
   const random = Math.floor(Math.random() * roasts.length);
-  if (message.author.id === "259758466942369794") return message.reply("**DANGER** *High dosage of chemotherapy, use only as instructed to combat extreme Habnal Cancer* :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: :radioactive: *Developed by Karolinska Institute Research and Development in Solborg, manufactured in Tripolia*");
   if (args[0] === "me") {
     message.reply(roasts[random]);
   } else if (!args.join(" ")) {
     message.reply("please specify who I need to roast");
 } else {
-  message.channel.sendMessage(args.join(" ") + ", " + roasts[random]);
+  message.channel.send(args.join(" ") + ", " + roasts[random]);
 }
 }
