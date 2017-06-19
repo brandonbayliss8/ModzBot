5b88110acecd05f3a01fcf1a75a9789f9e5555d1
ModzOn <modzongaming@gmail.com>
2017-06-19 19:37:56 +0200

    Update help.js

--- command/help.js
+++ command/help.js
@@ -1,11 +1,11 @@
 const help = require("../data/helpmsg.json");
 exports.run = (bot, message, args) => {
-  message.author.sendMessage(help.helpmsg1);
+  message.author.send(help.helpmsg1);
           setTimeout(() => {
-              message.author.sendMessage(help.helpmsg2);
+              message.author.send(help.helpmsg2);
           }, 250);
           setTimeout(() => {
-              message.author.sendMessage(help.helpmsg3);
+              message.author.send(help.helpmsg3);
           }, 500);
           message.reply(`Check your DMs for help!`);
   }
