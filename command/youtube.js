7dd5cf12a1d2b8b637f0662596c2253b756fc254
ModzOn <modzongaming@gmail.com>
2017-06-19 19:51:15 +0200

    Update youtube.js

--- command/youtube.js
+++ command/youtube.js
@@ -14,14 +14,13 @@ args = args.join(" ").substring(command.length + 1);
                     message.channel.sendMessage(`:no_entry_sign: **Error:**\n${error}`);
                     console.log(error);
                 } else {
-                    if (!result.items[0]["id"]) return message.reply(":no_entry_sign: **Error:** There was an issue getting that song.");
-                    if (result.items[0]["id"].videoId === undefined) return message.reply(":no_entry_sign: **Error: no videos found!**")
+                    if (result.items[0] === undefined) return message.reply(":no_entry_sign: **Error: no videos found!**")
                     let url = `https://www.youtube.com/watch?v=${result.items[0]["id"].videoId}`;
-                    message.channel.sendMessage(url);
+                    message.channel.send(url);
                 }
             });
          };
-         let query = message.content.replace('::youtube search', '');
+         let query = message.content.replace(':youtube search', '');
        searchargs(query);
      }
 }
