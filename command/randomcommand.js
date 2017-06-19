84a97ea79da91acfa3299648951ec22cf7373432
ModzOn <modzongaming@gmail.com>
2017-06-19 19:40:43 +0200

    Delete randomcommand.js

--- command/randomcommand.js
+++
@@ -1,5 +0,0 @@
-const Discord = require("Discord.js");
-exports.run = (bot, message, args) => {
-  var comm = ["\`::rockpaperscissors\`", "\`::8ball <question>\`", "\`::stab\`", "\`::about\`", "\`::usergame\`"]
-    message.reply("Here is a command for you!" + comm[Math.floor(Math.random() * comm.length)]);
-}
