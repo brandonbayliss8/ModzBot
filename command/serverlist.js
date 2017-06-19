d8fc6e221c3c4ef83831eebc854b4eb0d8d35a47
ModzOn <modzongaming@gmail.com>
2017-06-19 19:44:40 +0200

    Update serverlist.js

--- command/serverlist.js
+++ command/serverlist.js
@@ -1,3 +1,3 @@
 exports.run = (bot, message) => {
-  message.channel.sendMessage(`Here is a list of servers where I am currently in! \n\`\`\`CSS\n${bot.guilds.map(g => g.name).join(" \n")}\`\`\``);
+  message.channel.send(`Here is a list of servers where I am currently in! \n\`\`\`CSS\n${bot.guilds.map(g => g.name).join(" \n")}\`\`\``);
 }
