2427919b453dce829bc9ab28aa596470de0d7aed
ModzOn <modzongaming@gmail.com>
2017-06-19 19:35:31 +0200

    Update disable.js

--- command/disable.js
+++ command/disable.js
@@ -1,7 +1,23 @@
+const fs = require("fs");
+      data = JSON.parse(fs.readFileSync('./data/data.json', 'utf8')),
 exports.run = (bot, message, args) => {
   if (!args[0]) return;
   let command = args[0].toLowerCase();
   args = args.join(" ").substring(command.length + 1);
+  if (command === "welcome") {
+    /*if (!data[message.guild.id]){ data[message.guild.id] = {
+        welcomemessage: "disabled"
+      };
+    }else {
+        data[message.guild.id].welcomemessage = "disabled";
+      }
+
+      fs.writeFile('./data/data.json', JSON.stringify(data), (err) => {
+        if (err) console.error(err)
+      });
+      message.channel.send("Welcome message disabled!");*/
+      message.reply(`This command is currently disabled.`)
+    }
   if (command === "logs") {
     message.reply(`Would you like to disable the logs?\n*This will be canceled in 10 seconds. Type \`yes\` or \`no\`.*`).then(() => {
       message.channel.awaitMessages(m => m.author.id === message.author.id && ['yes', 'no'].includes(m.content), {time: 10000, maxMatches: 1})
@@ -9,12 +25,12 @@ exports.run = (bot, message, args) => {
       if (!collected.size) {
       return;
     } else if (collected.first().content.toLowerCase() === 'yes') {
-      message.channel.sendMessage(`Disabling the logs...`);
+      message.channel.send(`Disabling the logs...`);
       console.log(`${date} at ${time}: ${message.author.username} disabled the logs in ${message.guild.name}.`);
       message.guild.channels.find("name", `mod-log`).delete();
       message.reply(`Logs disabled!`)
     } else if (collected.first().content.toLowerCase() === 'no') {
-      message.channel.sendMessage("Returning...");
+      message.channel.send("Returning...");
     } else {
       message.reply("You didn't say `yes` or `no`. Returning...");
     }
