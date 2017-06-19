c84660c4c7d3cf44eee460b295297840cb92bd11
ModzOn <modzongaming@gmail.com>
2017-06-19 19:01:53 +0200

    Update bugreport.js

--- command/bugreport.js
+++ command/bugreport.js
@@ -2,7 +2,6 @@ exports.run = (bot, message, args) => {
   if (message.guild !== null) {
       var output = args.join(" ");
       
-      if (message.author.id === "191165862022086656") return console.log(`${message.createdAt}: ${message.author.username} tried to report a bug but was blacklisted`);//blacklist
       //if (message.author.id === "298144419088039946") return console.log(`${message.createdAt}: ${message.author.username} tried to report a bug but was blacklisted`)//blacklist
 
       bot.channels.get("301421032210956289").sendMessage("A bug report was received from " + "**" + message.member.guild.name + "**" + "\nUser who reported the bug: **" + message.author.username + "**" + "```\n" + output + "\n```");
