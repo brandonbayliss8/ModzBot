fe136260639e8283b2ac0ac0ffd34f834f6eb24b
ModzOn <modzongaming@gmail.com>
2017-06-19 13:11:19 +0200

    Update suggest.js

--- command/suggest.js
+++ command/suggest.js
@@ -2,7 +2,8 @@ exports.run = (bot, message, args) => {
   if (message.guild !== null) {
       var output = args.join(" ");
 
-      //if (message.author.id === "298144419088039946") return console.log(`${message.createdAt}: ${message.author.username} tried to report a bug but was blacklisted`)//blacklist
+      if (message.author.id === "191165862022086656") return console.log(`${message.createdAt}: ${message.author.username} tried to report a bug but was blacklisted`);//blacklist
+      //if (message.author.id === "") return console.log(`${message.createdAt}: ${message.author.username} tried to report a bug but was blacklisted`);//blacklist
 
       bot.channels.get("308533381048696832").sendMessage("A suggestion was submitted from " + "**" + message.member.guild.name + "**" + "```\n" + output + "\n```");
       bot.channels.get("307878217228877827").sendMessage(`**${message.author.username}** filled a suggestion \n \n User ID: **${message.author.id}** \n Server name: **${message.member.guild.name}** \n Server ID: **${message.member.guild.id}**`)
