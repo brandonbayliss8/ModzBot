2ae800414a4be89a0ea0767bd7a11bb5a121dffd
ModzOn <modzongaming@gmail.com>
2017-06-19 19:49:05 +0200

    Update userstats.js

--- command/userstats.js
+++ command/userstats.js
@@ -17,23 +17,29 @@ exports.run = (bot, message, args) => {
   let nick = {
     "null" : "No nickname"
   };
+  var user;
+  if (!message.mentions.users.first()) {
+    user = message.author;
+  } else {
+    user = message.mentions.users.first();
+  };
       const embed = new Discord.RichEmbed()
         embed.setColor(3447003)
-        .setAuthor(`${message.author.username}`, message.author.avatarURL)
-        .setTitle(`${message.author.username}` , "Information about this user!")
-        .setDescription(`Information about ${message.author.username}`)
-        .setThumbnail(message.author.avatarURL)
-        .addField(`• Username`, `${message.author.username}#${message.author.discriminator}`, true)
-        .addField(`• Account created`, `${moment(message.author.createdAt).format('DD MMMM YYYY')}`, true)
-        .addField(`• Status`, status[message.author.presence.status], true)
-        .addField(`• User ID`, `${message.author.id}`, true)
-        .addField(`• Bot?`, botuser[message.author.bot], true)
-        .addField(`• Server joined`, `${moment(message.member.joinedAt).format('DD MMMM YYYY')}`, true)
-        .addField(`• Nickname`, message.member.nickname != null ? message.member.nickname : "No nickname", true)
-        .addField(`• Game playing`, message.author.presence.game != null ? message.author.presence.game.name : "Nothing", true)
+        .setAuthor(`${user.username}`, user.avatarURL)
+        .setTitle(`${user.username}` , "Information about this user!")
+        .setDescription(`Information about ${user.username}`)
+        .setThumbnail(user.avatarURL)
+        .addField(`• Username`, `${user.username}#${user.discriminator}`, true)
+        .addField(`• Account created`, `${moment(user.createdAt).format('DD MMMM YYYY')}`, true)
+        .addField(`• Status`, status[user.presence.status], true)
+        .addField(`• User ID`, `${user.id}`, true)
+        .addField(`• Bot?`, botuser[user.bot], true)
+        .addField(`• Server joined`, `${moment(message.guild.member(user).joinedAt).format('DD MMMM YYYY')}`, true)
+        .addField(`• Nickname`, message.guild.member(user).nickname != null ? message.guild.member(user).nickname : "No nickname", true)
+        .addField(`• Game playing`, user.presence.game != null ? user.presence.game.name : "Nothing", true)
         .setFooter(`Generated on ${date}, at ${time}`)
 
-        message.channel.sendEmbed(embed);
+        message.channel.send({ embed });
 
-        console.log(`${message.createdAt}: ${message.author.username} requested userinfo about ${message.author.username}`);
+        console.log(`${message.createdAt}: ${user.username} requested userinfo about ${user.username}`);
 }
