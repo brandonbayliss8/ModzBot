43a421fe77399c9a95b8bf37cbdba6d72b614680
ModzOn <modzongaming@gmail.com>
2017-06-19 19:32:53 +0200

    Update avatar.js

--- command/avatar.js
+++ command/avatar.js
@@ -1,15 +1,19 @@
 const Discord = require("discord.js");
 exports.run = (bot, message, args) => {
   let img = message.mentions.users.first()
-  if (!args[0]) return message.reply(`Please mention a user!`);
-  if (!img) return message.reply(`Please mention a valid user!`);
-  if (img.avatarURL === null) {
+  if (!img) {
+  const embed = new Discord.RichEmbed()
+  .setImage(`${message.author.avatarURL}`)
+  .setFooter(`Avatar of ${message.author.username}#${message.author.discriminator} requested by ${message.author.username}#${message.author.discriminator}`)
+
+  message.channel.send({ embed });
+} else if (img.avatarURL === null) {
     message.channel.sendMessage("The mentioned user (" + img + ") does not have an avatar!")
   } else {
   const embed = new Discord.RichEmbed()
   .setImage(`${img.avatarURL}`)
   .setFooter(`Avatar of ${img.username}#${img.discriminator} requested by ${message.author.username}#${message.author.discriminator}`)
 
-  message.channel.sendEmbed(embed);
+  message.channel.send({ embed });
   }
 }
