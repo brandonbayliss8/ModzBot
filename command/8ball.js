77b0ca27c7b36bd6247ab08b9292f63bfd32b558
ModzOn <modzongaming@gmail.com>
2017-06-19 19:31:51 +0200

    ModzBot v1.2.5

--- command/8ball.js
+++ command/8ball.js
@@ -4,5 +4,5 @@ exports.run = (bot, message, args) => {
   if (question === "jetti") return message.channel.sendMessage("no fuk you kieb stronk");
   var answ = ["Yes", "No", "Why?", "Please stop, human", "Let's find it out!", "Maybe", "I don't know mate", "Definately", "Are you serious?", "For sure!", "Yes", "No", "Of course!", "Of course not"];
   if (!question) return message.reply(`Please ask me a question`)
-    message.channel.sendMessage(answ[Math.floor(Math.random() * answ.length)]);
+    message.channel.send(answ[Math.floor(Math.random() * answ.length)]);
 }
