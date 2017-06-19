875d974f5f312542604be476c6d5a86ae2741360
ModzOn <modzongaming@gmail.com>
2017-06-19 19:49:27 +0200

    Update userstatus.js

--- command/userstatus.js
+++ command/userstatus.js
@@ -13,8 +13,12 @@ exports.run = (bot, message, args) => {
     "dnd": "#ff0000",
     "offline": "#d8d8d8"
   };
-  if (!message.mentions.users.first()) return message.reply(`Please mention a user!`)
-  let user = message.mentions.users.first();
+  var user;
+  if (!message.mentions.users.first()) {
+    user = message.author;
+  } else {
+    user = message.mentions.users.first();
+  };
   const embed = new Discord.RichEmbed()
   const now = new Date();
   const date = moment(now).format("MMM/DD/YYYY");
@@ -22,6 +26,6 @@ exports.run = (bot, message, args) => {
     embed.setColor(colors[user.presence.status])
     .addField(`Status for **${user.username}#${user.discriminator}**`, `${status[user.presence.status]}`)
     .setFooter(`Got status on ${date} at ${time}`)
-    message.channel.sendEmbed(embed);
+    message.channel.send({ embed });
       //message.reply(`Status for **${user.username}#${user.discriminator}**: \`\`\`CSS\n${status[user.presence.status]} \`\`\``);
     }
