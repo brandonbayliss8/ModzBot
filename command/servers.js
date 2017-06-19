c497f48bd6d01ea8fd9c713562ec2cb345a257cd
ModzOn <modzongaming@gmail.com>
2017-06-19 19:44:56 +0200

    Update servers.js

--- command/servers.js
+++ command/servers.js
@@ -1,3 +1,3 @@
 exports.run = (bot, message) => {
-  message.channel.sendMessage(`I am currently in **${bot.guilds.size.toLocaleString()}** servers, with a total of **${bot.channels.size.toLocaleString()}** channels and **${bot.users.size.toLocaleString()}** users!`);
+  message.channel.send(`I am currently in **${bot.guilds.size.toLocaleString()}** servers, with a total of **${bot.channels.size.toLocaleString()}** channels and **${bot.users.size.toLocaleString()}** users!`);
 }
