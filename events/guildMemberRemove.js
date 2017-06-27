8848e1d32fbc4fdb2f9c039ea1cad292113c2cfd
ModzOn <modzongaming@gmail.com>
2017-06-27 18:49:48 +0200

    Add files via upload

---
+++ events/guildMemberRemove.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js")
+exports.run = (bot, member) => {
+  //Leave message
+  bot.on('guildMemberRemove', member => {
+    if (member === bot.user.id) return;
+    let guild = member.guild;
+    const embed = new Discord.RichEmbed()
+      embed.setColor('#CE0000')
+      .setAuthor(`Join log`)
+      .setDescription(`**${member}** left **${guild}**`)
+
+    var msg;
+    msg = `Bye ${member}!`;
+    guild.defaultChannel.sendMessage(msg);
+    console.log(`${member} left ${member.guild.name}`)
+    //bot.channels.get("305057938567987200").sendEmbed(embed);
+});
+}
