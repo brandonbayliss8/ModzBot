dbaab288b483eafa98ca2cde7798dde624b80697
ModzOn <modzongaming@gmail.com>
2017-06-27 17:52:57 +0200

    Update support.js

--- command/support.js
+++ command/support.js
@@ -1,3 +1,3 @@
 exports.run = (bot, message, args) => {
-  message.reply("Join my Discord server here! https://discord.gg/T2cvH6v")
+  message.reply("Join my Discord server here! https://discord.me/ModzBot")
 }
