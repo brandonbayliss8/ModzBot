8b97e4d4ec4518297a2df2f6466b54648cde0431
ModzOn <modzongaming@gmail.com>
2017-06-27 17:36:33 +0200

    Update birth.js

--- command/birth.js
+++ command/birth.js
@@ -1,3 +1,4 @@
+const moment = require('moment')
 exports.run = (bot, message, args) => {
-  message.reply(message.author.createdAt); //sends the date and time when the user created it's account
+  message.reply(`Your account was created on ${moment(message.author.createdAt).format("DD MMMM YYYY")}`); //sends the date and time when the user created it's account
 }
