a6d00d9b8a60e105e2b39106520adf069f6d81ef
ModzOn <modzongaming@gmail.com>
2017-06-27 17:44:17 +0200

    Update help.js

--- command/help.js
+++ command/help.js
@@ -1,11 +1,57 @@
-const help = require("../data/helpmsg.json");
+const help = require("../data/help.json");
+      Discord = require('discord.js');
 exports.run = (bot, message, args) => {
-  message.author.send(help.helpmsg1);
-          setTimeout(() => {
-              message.author.send(help.helpmsg2);
-          }, 250);
-          setTimeout(() => {
-              message.author.send(help.helpmsg3);
-          }, 500);
-          message.reply(`Check your DMs for help!`);
+  if (!args[0]) {
+    message.reply(`What would you like to get help with?\n*This will be canceled in 30 seconds. Options: \`all\`, \`moderation\` or \`music\`. Type \`cancel\` to cancel this command.*`).then(() => {
+      message.channel.awaitMessages(m => m.author.id === message.author.id && ['all', 'moderation', 'music', 'cancel'].includes(m.content), {time: 30000, maxMatches: 1})
+      .then(collected => {
+      if (!collected.size) {
+      return message.reply(`You didn't specify anything in time!`);
+    } else if (collected.first().content.toLowerCase() === 'all') {
+      message.author.send(help.helpALL);
+      message.reply(`Help sent. Check your DM's!`);
+    } else if (collected.first().content.toLowerCase() === 'moderation') {
+      message.author.send(help.helpADMIN);
+      message.reply(`Help sent. Check your DM's!`);
+    } else if (collected.first().content.toLowerCase() === 'music') {
+      message.author.send(help.helpMUSIC);
+      message.reply(`Help sent. Check your DM's!`);
+    } else if (collected.first().content.toLowerCase() === 'cancel') {
+      message.reply(`Command cancelled.`);
+    } else {
+      message.reply("You didn't specify anything in time! Returning...");
+    }
+    });
+    });
+  } else {
+    let command = args[0];
+    if (command === "all") {
+      message.author.send(help.helpALL);
+      message.reply(`Help sent. Check your DM's!`);
+    } else if (command === "moderation") {
+      message.author.send(help.helpADMIN);
+      message.reply(`Help sent. Check your DM's!`);
+    } else if (command === "music") {
+      message.author.send(help.helpMUSIC);
+      message.reply(`Help sent. Check your DM's!`);
+    } else if (!help[command]) {
+      message.reply(`Specified command (${command}) not found!`)
+    } else {
+      var info;
+      if (!help[command].info) {
+        info = "None";
+      } else {
+        info = help[command].info;
+      }
+	    const embed = new Discord.RichEmbed();
+
+      embed.setColor("RANDOM")
+      .setTitle(`Help for ${help[command].name}`)
+      .addField(`Description`, help[command].description)
+	    .addField(`Usage`, "`" + "::" + help[command].usage + "`")
+      .addField(`Optional info`, info)
+
+    message.channel.send({ embed });
+    }
   }
+}
