7f7be347ab1bad4e0d3074cd99ae2d888635ea97
Gavin Wainwright <gav.wainwright@gmail.com>
2017-06-27 21:07:33 -0400

    remove guildMemberRemove event from exports

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,17 +1,14 @@
 const { RichEmbed } = require("discord.js");
 exports.run = (bot, member) => {
-  //Leave message
-  bot.on('guildMemberRemove', member => {
-    if (member === bot.user.id) return;
-    let guild = member.guild;
-    const embed = new RichEmbed()
-        .setColor('#CE0000')
-        .setAuthor('Join log')
-        .setDescription(`**${member}** left **${guild}**`);
+  if (member === bot.user.id) return;
+  let guild = member.guild;
+  const embed = new RichEmbed()
+      .setColor('#CE0000')
+      .setAuthor('Join log')
+      .setDescription(`**${member}** left **${guild}**`);
 
-    let msg = `Bye ${member}!`;
-    guild.defaultChannel.send(msg);
-    console.log(`${member} left ${member.guild.name}`)
-    //bot.channels.get("305057938567987200").sendEmbed(embed);
-});
+  let msg = `Bye ${member}!`;
+  guild.defaultChannel.send(msg);
+  console.log(`${member} left ${member.guild.name}`)
+  //bot.channels.get("305057938567987200").sendEmbed(embed);
 }
