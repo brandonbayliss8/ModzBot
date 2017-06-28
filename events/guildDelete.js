7ba0173d3441d575545e7c146617c6174d18723d
Gavin Wainwright <gav.wainwright@gmail.com>
2017-06-27 21:11:38 -0400

    template literals boi

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,10 +1,8 @@
 exports.run = (bot, guild) => {
-    let toSend = [
-      "I've been removed from " + "\**" + guild.name + "\**",
-      "Guild ID: " + "\**" + guild.id + "\**",
-  "Guild Members Count: " + "\**" + guild.memberCount + "\**",
-    "Guild Region: " + "\**" + guild.region + "\**"
-  ];
+  let toSend = `I've been removed from **${guild.name}**
+Guild ID: **${guild.id}**
+Guild Member Count: **${guild.memberCount}**
+Guild Region: **${guild.region}**`;
 
   bot.channels.get("305383689464971264").send(toSend);
   bot.user.setGame(`::help ::about | In ${bot.guilds.size} servers!`);
