1fd555559e57817874759fcbfd246ae203820407
Sam Lawson <samilawson17@gmail.com>
2017-06-30 17:13:12 -0500

    Update serverlist.js
    See comment

--- command/serverlist.js
+++ command/serverlist.js
@@ -1,3 +1,3 @@
 exports.run = (bot, message) => {
-  message.channel.send(`Here is a list of servers where I am currently in! \n\`\`\`CSS\n${bot.guilds.map(g => g.name).join(" \n")}\`\`\``);
+  message.channel.send(`Here is a list of servers where I am currently in! \n\`\`\`CSS\n${bot.guilds.filter(g => g.name < 50).join(" \n")}\`\`\``); //add a filter to make sure the message doesnt break the char limit, that number is random
 }
